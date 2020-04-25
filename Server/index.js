d023edec433681ac335baf112242d9708b3692a5
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-04-26 00:41:43 +0430

    index.js completed

--- Server/index.js
+++ Server/index.js
@@ -15,3 +15,32 @@ mongoose
 const port = process.env.PORT || 5000;
 
 app.listen(port,()=>console.log(`Server is running on port ${port}`));
+
+//middleware for json
+app.use(express.json);
+
+//Routes add
+const postsRoutes = require('./routes/Posts');
+app.use('/api/posts',postsRoutes);
+
+// for 404 handeling
+app.use(
+    (req,res,next)=>{
+        const error = new Error('URL not found!');
+        error.status = 404;
+        next(error);
+    }
+);
+
+//General Error handeling
+app.use(
+    (error,req,res,next)=>{
+        res.status(error.status || 500);
+        res.send(
+            {
+                message: `Error : ${error.message}`,
+                error: error
+            }
+        );
+    }
+);
