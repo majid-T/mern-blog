30fbadf9f74a1e04d928f08c43a00750efbf6b29
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-04-27 03:32:17 +0430

    Pages are done!

---
+++ Client/src/pages/CreatePostPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CreatePost(){
+    return(
+        <div className='CreatePost'>
+            <h1>Create Post page</h1>
+        </div>
+    );
+}
+
+export default CreatePost;
---
+++ Client/src/pages/SinglePostPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SinglePostPage(){
+    return(
+        <div className='CreatePost'>
+            <h1>Single Post page</h1>
+        </div>
+    );
+}
+
+export default SinglePostPage;
