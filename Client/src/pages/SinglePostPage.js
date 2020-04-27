ae0576ac9593d2666169c4ec751052291f69b202
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-04-28 02:10:13 +0430

    SinglePostPage.js done!

--- Client/src/pages/SinglePostPage.js
+++ Client/src/pages/SinglePostPage.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import Container from 'react-bootstrap/Container';
+import axios from 'axios';
 
-function SinglePostPage(){
+
+function SinglePostPage(props){
+    const [post,setPost] = useState({});
+
+    const postId = props.match.params.id;
+    useEffect(()=>{
+        axios
+        .get(`http:localhost:5000/api/posts/get/${postId}`)
+        .then((res)=>setPost(res.data.post))
+        .catch((err)=>console.log(err));
+    },[postId]);
+
     return(
-        <div className='CreatePost'>
-            <h1>Single Post page</h1>
-        </div>
+        <Container>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+            <p>Author:{post.author}</p>
+        </Container>
     );
 }
 
