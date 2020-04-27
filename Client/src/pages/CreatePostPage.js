ff5cd21f734c6bae9244cc640265c5e8601cf27f
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-04-28 02:03:20 +0430

    CreatePostPage.js done!

--- Client/src/pages/CreatePostPage.js
+++ Client/src/pages/CreatePostPage.js
@@ -7,11 +7,36 @@ import axios from 'axios';
 function CreatePost(props){
     const [formData,setFormData] = useState({title:'',body:'',author:''});
 
-    const handle
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        await axios
+        .post('http://localhost:5000/api/posts/create')
+        .then((response)=>console.log(response))
+        .catch((err)=>console.log(err));
+        props.history.push('/');
+    };
+
     return(
-        <div className='CreatePost'>
-            <h1>Create Post page</h1>
-        </div>
+        <Container>
+            <h1 className='mb-4'>Create Post</h1>
+            <Form>
+                <Form.Group controlId='title'>
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control type='text' placeholder='Post Title' value={formData.title} 
+                    onChange={(e)=> setFormData({...formData,title:e.target.value})}/>
+                </Form.Group>
+                <Form.Group controlId='body'>
+                    <Form.Label>Body</Form.Label>
+                    <Form.Control type='text' placeholder='Post Body' value={formData.body} 
+                    onChange={(e)=> setFormData({...formData,body:e.target.value})}/>
+                </Form.Group>
+                <Form.Group controlId='author'>
+                    <Form.Label>Author</Form.Label>
+                    <Form.Control type='text' placeholder='Post Author' value={formData.author} 
+                    onChange={(e)=> setFormData({...formData,author:e.target.value})}/>
+                </Form.Group>
+            </Form>
+        </Container>
     );
 }
 
